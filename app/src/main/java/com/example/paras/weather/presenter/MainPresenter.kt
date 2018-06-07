1fee07c4dd06e39052a8825f17e66b5c324274fb
Paras <khandelwalparas8@gmail.com>
2018-06-07 22:15:58 +0530

    MainPresenter remaining

--- app/src/main/java/com/example/paras/weather/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/paras/weather/presenter/MainPresenter.kt
@@ -2,11 +2,14 @@ package com.example.paras.weather.presenter
 
 import com.example.paras.weather.BuildConfig
 import com.example.paras.weather.OpenWeatherAPI
+import com.example.paras.weather.data.WeatherResponse
 import com.example.paras.weather.interceptor.OpenWeatherInterceptor
 import com.example.paras.weather.ui.ErrorTypes
 import com.example.paras.weather.ui.MainView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
-
 class MainPresenter(val view : MainView){
 
 @Inject lateinit var api : OpenWeatherAPI
@@ -17,6 +20,21 @@ class MainPresenter(val view : MainView){
             return
         }
         view.showSpinner()
-        api.dailyForecast(cityName,7)
+        api.dailyForecast(cityName,7).enqueue(object : Callback<WeatherResponse>{
+
+            override fun onResponse(call: Call<WeatherResponse>?, response: Response<WeatherResponse>) {
+              response.body()?.let{
+                //createListForView(it)
+                  view.hideSpinner()
+              }?: view.showErrorToast(ErrorTypes.NO_RESULT_FOUND)
+            }
+
+            override fun onFailure(call: Call<WeatherResponse>?, t: Throwable) {
+                view.showErrorToast(ErrorTypes.CALL_ERROR)
+                t.printStackTrace()
+            }
+
+
+            })
     }
 }
